4a39684bb90d4f0ea18a0cf34395affe4ff5e46a
ignatiqq <zzzcap3@gmail.com>
2022-12-23 22:11:31 +0300

    remove deprecated comment

--- src/client/libs/Microfrontends/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/client/libs/Microfrontends/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -37,34 +37,3 @@ const DynamicModuleLoader = <ModuleProps,>({
 };
 
 export default DynamicModuleLoader;
-
-
-// LEGACY::
-
-// const [isLoading, setIsLoading] = useState(true);
-// const [error, setError] = useState('');
-
-// const renderFnRef = React.useRef<FederatedComponent<ModuleProps> | null>();
-
-// useEffect(() => {
-// 	const loadModuleFn = async function () {
-// 		try {
-// 			const Component = requestForModule<ModuleProps>(containerName, module);
-
-// 		} catch (error) {
-// 			setError((error as Error).message);
-// 			renderFnRef.current = null;
-// 		} finally {
-// 			setIsLoading(false);
-// 		}
-// 	};
-
-// 	loadScript(url, {
-// 		onLoad: () => {
-// 			loadModuleFn();
-// 		},
-// 		onError: () => {
-// 			setError(`Some error occured while ${containerName} MFE loading`);
-// 		},
-// 	});
-// }, []);
